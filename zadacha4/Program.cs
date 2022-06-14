04c9b41a2ad12c797e03405d1ada7e9c12b3e4a5
kgs90 <kgs90@inbox.ru>
2022-06-14 18:53:17 +0300

    zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,27 @@
+﻿int[,,] array = new int[4,4,5];
+int[] check = new int[100];
+int i = 0, j = 0, k = 0, o = 0;
+Random rand = new Random();
+
+for (i = 0; i < 4; i++)
+    for (j = 0; j < 4; j++)
+        for (k = 0; k < 5; k++) 
+        {
+            array[i,j,k] = rand.Next(10, 99);
+            for (o = 0; o < i*j*k; o++)
+                while (check[o] == array[i,j,k]) 
+                {
+                    array[i,j,k] = rand.Next(10, 99);
+                    Console.WriteLine("повторение");
+                }
+                    
+        }
+
+Console.WriteLine("Сгенерированный массив 4x4x5: ");
+
+for (i = 0; i < 4; i++)
+    for (j = 0; j < 4; j++)
+        for (k = 0; k < 5; k++) 
+        {
+            Console.WriteLine("array[" + i + "," + j + ","  + k + "] = " + array[i,j,k]);
+        }
