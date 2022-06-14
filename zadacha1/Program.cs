e1a89ad5e6f34599fd2a0ef9153f658cd50a9cb7
kgs90 <kgs90@inbox.ru>
2022-06-14 18:35:07 +0300

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,43 @@
+﻿int[,] array = new int[1000,1000];
+double[] avg = new double[1000];
+int i = 0, j = 0, cap = 0, k = 0;
+Random rand = new Random();
+
+Console.WriteLine("Задать массив MxN  через пробел: ");
+int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+for (i = 0; i < size[1]; i++)
+    for (j = 0; j < size[0]; j++)
+        array[i,j] = rand.Next(100);
+
+Console.WriteLine("Сгенерированный массив: " + size[0] + "x" + size[1]);
+
+for (i = 0; i < size[1]; i++) 
+{
+    for (j = 0; j < size[0]; j++)
+        if (array[i,j] < 10) Console.Write(" " + array[i,j] + " ");
+        else Console.Write(array[i,j] + " ");
+    Console.WriteLine();
+}
+
+for (i = 0; i < size[1]; i++)
+    for (j = 0; j < size[0] ; j++)
+        for (k = 0; k < size[0]; k++) 
+        {
+            if (array[i,j] > array[i,k]) 
+            {
+                cap = array[i,k];
+                array[i,k] = array[i,j];
+                array[i,j] = cap;
+            }
+        }
+
+Console.WriteLine("Отсортированный массив: " + size[0] + "x" + size[1]);
+
+for (i = 0; i < size[1]; i++) 
+{
+    for (j = 0; j < size[0]; j++)
+        if (array[i,j] < 10) Console.Write(" " + array[i,j] + " ");
+        else Console.Write(array[i,j] + " ");
+    Console.WriteLine();
+}
